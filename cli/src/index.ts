b9f5b5b24eac530a518f4bcf80f8532f4f19a5bf
TheWindX <453588006@qq.com>
2019-10-15 10:12:27 +0100

    chore: gitignore

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -4,26 +4,22 @@ import clone from './clone';
 const util = script.util;
 const log = console.log;
 
-
 let argv = util.yargs
-  .command('clone', 'choose a template for your project', () => {
-    clone();
-    util.process.exit(0);
-  })
   .example('jsrock clone', ': choose a template for your project')
   .example('jsrock distribute', ': publish your builded project')
   .demandCommand()
   .argv
 
-// } else if (!script[subCommand]) {
-//   log(util.chalk.redBright(`$\n\n\ncannot understand: \n${cmdStr}`));
-// } else {
-
 let subCommand = argv._[0];
 
-if (subCommand && script[subCommand]) {
-  script[subCommand]();
-  util.process.exit(0);
+if (subCommand) {
+  if (subCommand == 'clone') {
+    clone();
+  } else if (script[subCommand]) {
+    script[subCommand]();
+    util.process.exit(0);
+  } else {
+    log(util.chalk.redBright(`$\n\n\ncannot understand\n`));
+  }
 }
 
-log(util.chalk.redBright(`$\n\n\ncannot understand\n`));
